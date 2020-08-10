7469ed98a2743fde5799734b8493961b525879b7
William Bogans <william.bogans@williambogans.dev>
2020-08-10 13:59:15 -0400

    home screen is fine

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -10,6 +10,7 @@ import {
 } from 'react-native';
 import { ContactContext } from '../context/ContactContext';
 import moment from 'moment';
+
 const HomeScreen = ({ navigation }) => {
   const { contacts, deleteContact } = useContext(ContactContext);
 
@@ -24,41 +25,13 @@ const HomeScreen = ({ navigation }) => {
         <View />
         <Text>
           Reminder set for:
-          <Text
-            style={{
-              fontSize: 18,
-              color: '#799ead',
-              fontWeight: '300',
-            }}
-          >
+          <Text>
             {' '}
             {moment(date).format('L')} at {moment(date).format('LT')}
           </Text>
         </Text>
-        <TouchableOpacity
-          style={{
-            position: 'absolute',
-            top: 1,
-            right: 10,
-            margin: 5,
-            backgroundColor: '#716992',
-            borderRadius: '5',
-            height: 22,
-            alignSelf: 'flex-end',
-            textAlign: 'center',
-          }}
-          onPress={() => deleteContact(id)}
-        >
-          <Text
-            style={{
-              color: '#fcf7e1',
-              paddingTop: '1%',
-              paddingBottom: '1%',
-            }}
-          >
-            {' '}
-            Delete{' '}
-          </Text>
+        <TouchableOpacity onPress={() => deleteContact(id)}>
+          <Text> Delete </Text>
         </TouchableOpacity>
       </View>
     );
@@ -66,66 +39,13 @@ const HomeScreen = ({ navigation }) => {
 
   if (!contacts.length) {
     return (
-      <SafeAreaView
-        style={{
-          backgroundColor: '#fcf7e1',
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
+      <SafeAreaView>
         <View>
-          <Text
-            style={{
-              fontSize: 30,
-              color: '#716992',
-              textAlign: 'center',
-              fontWeight: '700',
-            }}
-          >
-            Welcome to nTouch!
-          </Text>
-          <Image
-            style={{ width: 300, height: 300 }}
-            source={{
-              uri:
-                'https://i.pinimg.com/originals/ab/53/c3/ab53c3258caa5c3c691b4de46cb5ad88.gif',
-            }}
-          />
+          <Text>Welcome to nTouch!</Text>
         </View>
-        <Text
-          style={{
-            fontSize: 25,
-            alignItems: 'center',
-            textAlign: 'center',
-            color: '#799ead',
-          }}
-        >
-          You haven't added any contacts yet.
-        </Text>
-        <TouchableOpacity
-          style={{
-            backgroundColor: '#abc7b9',
-            width: '80%',
-            maxWidth: 500,
-            height: 100,
-            borderRadius: 10,
-            display: 'flex',
-            alignSelf: 'center',
-            justifyContent: 'center',
-            marginTop: 20,
-          }}
-          onPress={() => navigation.navigate('Add Contact')}
-        >
-          <Text
-            style={{
-              textAlign: 'center',
-              color: '#716992',
-              fontSize: 30,
-            }}
-          >
-            Create new Contact
-          </Text>
+        <Text>You haven't added any contacts yet.</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Add Contact')}>
+          <Text>Create new Contact</Text>
         </TouchableOpacity>
       </SafeAreaView>
     );
