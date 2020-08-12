4681b82055bec36b4512937520ad4a05731ae08a
William Bogans <william.bogans@williambogans.dev>
2020-08-12 06:38:57 -0400

    can retrieve contacts but, need to save to context

--- src/components/ContactListScreen.js
+++ src/components/ContactListScreen.js
@@ -1,7 +1,10 @@
 import React, { useContext } from 'react';
 import { View, Text } from 'react-native';
+import { ContactContext } from '../context/ContactContext';
 
 function ContactListScreen() {
+  const { userContacts } = useContext(ContactContext);
+  console.log(userContacts);
   return (
     <View>
       <Text></Text>
--- src/utils/useGetContacts.js
+++ src/utils/useGetContacts.js
@@ -27,5 +27,5 @@ export default () => {
     })();
   }, []);
 
-  return { data, error };
+  return { error };
 };
